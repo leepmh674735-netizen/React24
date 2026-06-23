e4401c052428d27c3f7713ad66a7be59f7fb6b4f
Yubin <leepmh674735@gmail.com>
2026-06-23 19:01:01 +0900

    12

---
+++ hello-react-project/ReactStudy/react06-hook-enhanced/src/components/UseFormStatusExam.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
+
+async function submitForm(formData) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${formData.get("name")}님의 요청이 완료되었습니다.`);
+        }, 1000);
+    });
+}
+
+const SubmitButton = () => {
+    const { pending, data, method, action } = useFormStatus();
+    console.log('data', data);
+    console.log('method', method);
+    console.log('action', action);
+
+    return (
+        <button type="submit" disabled={pending}>
+            {pending ? "제출 중 ..." : "제출"}
+        </button>
+    );
+};
+
+const UseFormStatusExam = () => {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (formData) => {
+        const result = await submitForm(formData);
+        setMessage(result);
+    };
+
+    return (
+        <>
+            <h2>useFormStatus 사용하기</h2>
+            <form action={handleSubmit}>
+                <label>
+                    이름 :
+                    <input type="text" name="name" placeholder="이름입력" />
+                </label>
+                <SubmitButton />
+            </form>
+            {message && <p>{message}</p>}
+        </>
+    );
+};
+
+export default UseFormStatusExam;
