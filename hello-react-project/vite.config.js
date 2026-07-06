ca30daad8b00963ff38ec9d361a8fe7e38c80689
Yubin <leepmh674735@gmail.com>
2026-07-06 14:36:55 +0900

    build: configure rollup multi-page inputs in vite.config.js

--- hello-react-project/vite.config.js
+++ hello-react-project/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        project01: resolve(__dirname, 'project01.html'),
+        project02: resolve(__dirname, 'project02.html'),
+        project03: resolve(__dirname, 'project03.html'),
+        project04: resolve(__dirname, 'project04.html'),
+        project05: resolve(__dirname, 'project05.html'),
+      }
+    }
+  }
 })
